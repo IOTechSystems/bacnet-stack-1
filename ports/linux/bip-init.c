31aa59f9b408adc897f77f1590d661d845c691ad
FraserG01 <fraser@iotechsys.com>
2021-03-23 16:13:08 +0000

    XRT-563: reverted test changes

--- ports/linux/bip-init.c
+++ ports/linux/bip-init.c
@@ -884,12 +884,7 @@ bool bip_init(char *ifname)
     }
     /* bind the socket to the local port number and IP address */
     sin.sin_family = AF_INET;
-#ifdef _AZURESPHERE_
-   // sin.sin_addr.s_addr = inet_addr("192.168.4.63");
     sin.sin_addr.s_addr = htonl(INADDR_ANY);
-#else
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
-#endif
     sin.sin_port = BIP_Port;
     memset(&(sin.sin_zero), '\0', sizeof(sin.sin_zero));
     status =
